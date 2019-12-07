cffb18bf12d95f0596de4ceb29fd5bf94bc1c37b
Abdelrahman Mounir <a.mounir.112@gmail.com>
2019-12-07 15:22:29 +0200

    added files

---
+++ Software Metrics Project/CalculateFP.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software_Metrics_Project
+{
+    class CalculateFP
+    {
+        public int UFP;
+        public int DI;
+        public double TCF;
+        public double FP;
+        public double LOC;
+
+        public Dictionary<Tuple<string, string>, int> complexityTable;
+        public Dictionary<string, int> TCF_Value;
+        public Dictionary<string, int> languageComplexity;
+
+        void InitializeComplexityTable()
+        {
+            Tuple<string, string> param = new Tuple<string, string>("External Input", "Simple");
+            complexityTable[param] = 3;
+            param = new Tuple<string, string>(param.Item1, "Average");
+            complexityTable[param] = 4;
+            param = new Tuple<string, string>(param.Item1, "Complex");
+            complexityTable[param] = 6;
+            
+            param = new Tuple<string, string>("External Output", "Simple");
+            complexityTable[param] = 4;
+            param = new Tuple<string, string>(param.Item1, "Average");
+            complexityTable[param] = 5;
+            param = new Tuple<string, string>(param.Item1, "Complex");
+            complexityTable[param] = 7;
+
+            param = new Tuple<string, string>("External Inquiry", "Simple");
+            complexityTable[param] = 3;
+            param = new Tuple<string, string>(param.Item1, "Average");
+            complexityTable[param] = 4;
+            param = new Tuple<string, string>(param.Item1, "Complex");
+            complexityTable[param] = 6;
+
+            param = new Tuple<string, string>("Internal Logical Files", "Simple");
+            complexityTable[param] = 7;
+            param = new Tuple<string, string>(param.Item1, "Average");
+            complexityTable[param] = 10;
+            param = new Tuple<string, string>(param.Item1, "Complex");
+            complexityTable[param] = 15;
+
+            param = new Tuple<string, string>("External Interface Files", "Simple");
+            complexityTable[param] = 5;
+            param = new Tuple<string, string>(param.Item1, "Average");
+            complexityTable[param] = 7;
+            param = new Tuple<string, string>(param.Item1, "Complex");
+            complexityTable[param] = 10;
+        }
+
+
+        void InitializeTCF()
+        {
+            TCF_Value["No Influence"] = 0;
+            TCF_Value["Incidental"] = 1;
+            TCF_Value["Moderate"] = 2;
+            TCF_Value["Average"] = 3;
+            TCF_Value["Significant"] = 4;
+            TCF_Value["Essential"] = 5;
+        }
+
+        void InitializeComplexity()
+        {
+            languageComplexity["Assembly Language"] = 320;
+            languageComplexity["C"] = 128;
+            languageComplexity["COBOL/Fortan"] = 105;
+            languageComplexity["Pascal"] = 90;
+            languageComplexity["Ada"] = 70;
+            languageComplexity["C++"] = 64;
+            languageComplexity["Visual Basic"] = 32;
+            languageComplexity["Object-Oriented Languages"] = 30;
+            languageComplexity["Smalltalk"] = 22;
+            languageComplexity["Code Generators (PowerBuilder)"] = 15;
+            languageComplexity["SQL/Oracle"] = 12;
+            languageComplexity["Spreadsheets"] = 6;
+            languageComplexity["Graphical Languages (icons)"] = 4;
+        }
+
+        public CalculateFP()
+        {
+            complexityTable = new Dictionary<Tuple<string, string>, int>();
+            InitializeComplexityTable();
+            TCF_Value = new Dictionary<string, int>();
+            InitializeTCF();
+            languageComplexity = new Dictionary<string, int>();
+            InitializeComplexity();
+        }
+        
+        public int CalculateUFP(List<Tuple<string, string, int>> data)
+        {
+            UFP = 0;
+            foreach(Tuple<string, string, int> element in data)
+            {
+                Tuple<string, string> key = new Tuple<string, string>(element.Item1, element.Item2);
+                int weight = complexityTable[key];
+                UFP += weight * element.Item3;
+            }
+
+            return UFP;
+        }
+
+        public double CalculateTCF(List<string> data)
+        {
+            DI = 0;
+
+            foreach(string element in data)
+            {
+                DI += TCF_Value[element];
+            }
+
+            TCF = 0.65 + 0.01 * DI;
+
+            return TCF;
+        }
+
+        public double CalculateFPValue()
+        {
+            FP = UFP * TCF;
+            return FP;
+        }
+
+        public double CalculateLOC(string language)
+        {
+            LOC = FP * languageComplexity[language];
+            return LOC;
+        }
+    }
+}
---
+++ Software Metrics Project/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software_Metrics_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateFP obj = new CalculateFP();
+            List<Tuple<string, string, int>> data = new List<Tuple<string, string, int>>();
+            data.Add(Tuple.Create("External Input", "Average", 24));
+            data.Add(Tuple.Create("External Output", "Average", 16));
+            data.Add(Tuple.Create("External Inquiry", "Average", 22));
+            data.Add(Tuple.Create("Internal Logical Files", "Average", 4));
+            data.Add(Tuple.Create("External Interface Files", "Average", 2));
+
+            Console.WriteLine(obj.CalculateUFP(data));
+            
+            List<string> GSC = new List<string>();
+            List<string> arr = new List<string>();
+            arr.Add("No Influence");
+            arr.Add("Incidental");
+            arr.Add("Moderate");
+            arr.Add("Average");
+            arr.Add("Significant");
+            arr.Add("Essential");
+            GSC.Add(arr[2]);
+            GSC.Add(arr[0]);
+            GSC.Add(arr[5]);
+            GSC.Add(arr[5]);
+            GSC.Add(arr[2]);
+            GSC.Add(arr[4]);
+            GSC.Add(arr[3]);
+            GSC.Add(arr[5]);
+            GSC.Add(arr[4]);
+            GSC.Add(arr[5]);
+            GSC.Add(arr[4]);
+            GSC.Add(arr[3]);
+            GSC.Add(arr[4]);
+            GSC.Add(arr[5]);
+
+            Console.WriteLine(obj.CalculateTCF(GSC) + " " + obj.DI);
+            Console.WriteLine(obj.CalculateFPValue() + " " + obj.UFP + " " + obj.TCF);
+
+        }
+    }
+}
